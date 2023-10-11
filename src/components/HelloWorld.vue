4c192c8f5d9a03c3c353c57e4ce88d8a5b4aefb4
Le Duy <duyla@hopee.com.vn>
2023-10-11 22:18:56 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,83 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <button @click="readNotice">{{ countNotice }}</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Echo from 'laravel-echo';
+
+
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      countNotice: 0,
+      loginToken: 'eyJraWQiOiJGY2hCY1B6U092REpCb0tFWTNSZjRGSkFkaElGSGNORGJYZ2tKZFh0azAwPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIxMzgxZDRjZi03ZTY2LTQ2NDktYTgxNC1hYjdhOTAwMTg3OTIiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtbm9ydGhlYXN0LTEuYW1hem9uYXdzLmNvbVwvYXAtbm9ydGhlYXN0LTFfVzlucno3SkNoIiwiY2xpZW50X2lkIjoiM3M3ZjdvMDk4a2pzYTQ3NDhoNGhyZWw4MmIiLCJvcmlnaW5fanRpIjoiNTFjYjVmNzgtYzBhYS00NDY2LWFiM2QtYTQzNzFkYTYyYjQ1IiwiZXZlbnRfaWQiOiI2MjgyOTNkNi01YmE2LTRjMjQtYTNmMS0zODlhNWQzNzNjZDkiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIiwiYXV0aF90aW1lIjoxNjk3MDIyMjE2LCJleHAiOjE2OTcwMjU4MTYsImlhdCI6MTY5NzAyMjIxNiwianRpIjoiMDRjZDg2YzctZTNhOS00OWI1LWI3NmMtODI5MTU3YjExNmQyIiwidXNlcm5hbWUiOiIxMzgxZDRjZi03ZTY2LTQ2NDktYTgxNC1hYjdhOTAwMTg3OTIifQ.m7bn9GUFENojtcuZ4OCjITMEX3ty4kSN40A4_W1Ppf90_Ic3ns-tQiqb-nI_xAm9itFnlaVzbzfzOuSswVuDshMYxBVvanbX_ySK16GzgZxJ9sTD3gK5C9TN04iZI_6VvQr7Npwv29gYXBoVX6Uz66x2la1lZe7HOa2G2NLg2cMF9dEJNs2k9FbvAGogfu6Hr3XgGlAZsaTeB6kX2NEAujQRGIF6CY1dmQsVzTgif1kNVzpzPvFD-UKlHY5ZE6bf-3fSheTtbul6gkh9rJhxQhFL_EzZHE5gy613mpwTklnH5r2-2wzPvL5mWap0mZjZH8mMQgOc2Im1BU5DtVzkpA'
+    };
+  },
   props: {
     msg: String
+  },
+  created() {
+    this.getCountNotice();
+    this.getListNotice();
+  },
+  mounted() {
+    const echo = new Echo({
+      broadcaster: 'socket.io',
+      host: 'http://localhost:6001'
+    });
+   
+    echo.join('notice.1')
+    .listen('NoticeEvent', (notice) => {
+      if (notice.data.recciverUserId == 1) {
+        this.countNotice = notice.data.countNotice;
+      }
+    })
+  },
+  methods: {
+    getCountNotice() {
+      axios.post('http://localhost:8010/api/v1/notice/count', [], {
+        headers: {
+          'Authorization': `Bearer ${this.loginToken}`
+        }
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    },
+    readNotice() {
+      axios.post('http://localhost:8010/api/v1/notice/read', [], {
+        headers: {
+          'Authorization': `Bearer ${this.loginToken}`
+        }
+      })
+        .then(response => {
+          console.log(response);
+          this.getListNotice();
+        })
+        .catch(error => {
+          console.error('API Error:', error);
+        });
+    },
+    getListNotice() {
+      axios.post('http://localhost:8010/api/v1/notice', [], {
+        headers: {
+          'Authorization': `Bearer ${this.loginToken}`
+        }
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    },
   }
 }
 </script>
